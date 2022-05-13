0685f20c833e8793f9dfbe11e314a3627ac54c81
mariamust <bella.torn@yandex.ru>
2022-05-13 13:10:30 +0500

    CSV commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,10 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -28,11 +17,9 @@ public class Main {
     public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
 
-        String fileName = "data.xml";
+        String fileName = "data.csv";
 
-//        List<Employee> list = parseCSV(columnMapping, fileName);
-
-        List<Employee> list = parseXML("data.xml");
+        List<Employee> list = parseCSV(columnMapping, fileName);
 
         String json = (String) listToJson(list);
 
@@ -53,52 +40,16 @@ public class Main {
         return null;
     }
 
-    public static List<Employee> parseXML(String xmlFileName){
-
-        try {
-            List<Employee> list = new ArrayList<>();
-
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(new File(xmlFileName));
-
-            Node root = doc.getDocumentElement();
-            NodeList nodeList = root.getChildNodes();
-
-            for (int i = 0; i < nodeList.getLength(); i++) {
-
-                Node node = nodeList.item(i);
-
-                if (node.ELEMENT_NODE == node.getNodeType()) {
-
-                    Element employee = (Element) node;
-
-                    long id = Long.parseLong(employee.getElementsByTagName("id").item(0).getTextContent());
-                    String firstName = employee.getElementsByTagName("firstName").item(0).getTextContent();
-                    String lastName = employee.getElementsByTagName("lastName").item(0).getTextContent();
-                    String country = employee.getElementsByTagName("country").item(0).getTextContent();
-                    int age = Integer.parseInt(employee.getElementsByTagName("age").item(0).getTextContent());
-
-                    list.add(new Employee(id, firstName, lastName, country, age));
-                }
-            }
-            return list;
-        } catch (IOException | SAXException | ParserConfigurationException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-
-    public static <T> Object listToJson(List<T> list) {
+    public static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        Type listType = new TypeToken<List<T>>() {
+        Type listType = new TypeToken<List<Employee>>() {
         }.getType();
         return gson.toJson(list, listType);
     }
 
     public static void writeString(String json) {
-        try (FileWriter fileWriter = new FileWriter("data2.json")) {
+        try (FileWriter fileWriter = new FileWriter("data.json")) {
             fileWriter.write(json);
             fileWriter.flush();
         } catch (IOException e) {
